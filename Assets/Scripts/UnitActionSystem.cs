58f12a2fc614890a1f628510e751f2cedf4d0493
IFernandezAD <ifernandezad@gmail.com>
2022-12-19 18:07:32 +0100

    Unit Selection Script ans Visuals enabled

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
 
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     void Update()
     {
-        if (TryHandleUnitSelection()) return;
-
         if (Input.GetMouseButtonDown(0))
         {
+            if (TryHandleUnitSelection()) return;
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
@@ -25,7 +37,7 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
@@ -34,5 +46,14 @@ public class UnitActionSystem : MonoBehaviour
 
     }
 
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
 
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
+    }
 }
