a8e832d0990c57e6ea9e6316a0665684317f4907
IFernandezAD <ifernandezad@gmail.com>
2022-12-22 17:35:17 +0100

    Grid detects what unit is on

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,21 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
     [SerializeField] private Animator unitAnimator;
-    private Vector3 targetPosition;
 
+    private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
     }
 
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+
     private void Update()
     {
-        
+
 
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
@@ -25,7 +30,7 @@ public class Unit : MonoBehaviour
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
             float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward,moveDirection,rotateSpeed*Time.deltaTime);
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
 
             unitAnimator.SetBool("IsWalking", true);
         }
@@ -33,6 +38,15 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool("IsWalking", false);
         }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+
+        if (newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
     }
 
     public void Move(Vector3 targetPosition)
--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -14,6 +14,7 @@ public class UnitActionSystem : MonoBehaviour
     {
         if (Instance != null)
         {
+            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
             Destroy(gameObject);
             return;
         }
