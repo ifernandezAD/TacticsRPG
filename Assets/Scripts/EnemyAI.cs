3e38053af6bd89dbcee9e9bf2f041fadd39fcd60
IFernandezAD <ifernandezad@gmail.com>
2023-01-05 13:18:30 +0100

    Simple Enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy,
+    }
+
+    private State state;
     private float timer;
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
+    }
 
     private void Start()
     {
@@ -19,18 +30,81 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
+        switch (state)
         {
-            TurnSystem.Instance.NextTurn();
+            case State.WaitingForEnemyTurn:
+                break;
+
+            case State.TakingTurn:
+
+                timer -= Time.deltaTime;
+                if (timer <= 0f)
+                {
+                    if (TryTakeEnemyAIAction(SetStateTakingTurn))
+                    {
+                        state = State.Busy;
+                    } else
+                    {
+                        //No more enemy actions, ending turn
+                        TurnSystem.Instance.NextTurn();
+                    }                            
+                }
+
+                break;
+
+            case State.Busy:
+                break;
         }
 
     }
 
-    private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
+    private void SetStateTakingTurn()
     {
-        timer = 2f;
+        timer = 0.5f;
+        state = State.TakingTurn;
     }
 
+    private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
+    {
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            state = State.TakingTurn;
+            timer = 2f;
+        }
+    }
 
+    private bool TryTakeEnemyAIAction(Action onEnemyAIActionComplete)
+    {
+        Debug.Log("Take Enemy AI Action!");
+        foreach (Unit enemyUnit in UnitManager.Instance.GetEnemyUnitList())
+        {
+            if (TryTakeEnemyAIAction(enemyUnit, onEnemyAIActionComplete))
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    private bool TryTakeEnemyAIAction(Unit enemyUnit, Action onEnemyAIActionComplete)
+    {
+        SpinAction spinAction = enemyUnit.GetSpinAction();
+
+
+        GridPosition actionGridPosition = enemyUnit.GetGridPosition();
+
+        if (!spinAction.IsValidActionGridPosition(actionGridPosition))
+        {
+            return false;
+        }
+        if (!enemyUnit.TrySpendActionPointsToTakeAction(spinAction))
+        {
+            return false;
+        }
+
+        Debug.Log("Spin Action!");
+        spinAction.TakeAction(actionGridPosition, onEnemyAIActionComplete);
+        return true;
+    }
 }
